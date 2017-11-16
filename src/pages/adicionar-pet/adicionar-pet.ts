43601abcdc1739cf0226e92d9d4a79a454fd3e85
marlon <kurtyebsb@gmail.com>
2017-11-16 19:28:42 -0200

    cadastrando no firebase

--- src/pages/adicionar-pet/adicionar-pet.ts
+++ src/pages/adicionar-pet/adicionar-pet.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, AlertController, NavParams} from 'ionic-angular';
+
+import { AngularFireDatabase } from 'angularfire2/database'
+import {Post} from "../../models/post";
+
 
 @Component({
   selector: 'page-adicionar-pet',
   templateUrl: 'adicionar-pet.html'
 })
+
+
+
 export class AdicionarPetPage {
 
-  constructor(public navCtrl: NavController) {
+  post = { } as Post;
+
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController,
+              private afDatabase: AngularFireDatabase
+              ) {
+
   }
-  
+
+
+
+    addPost() {
+        this.afDatabase.list('post/').push(this.post);
+    }
+
 }
