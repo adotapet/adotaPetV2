32f2c2643543d824b42eb1eaf5fd79f6d45f809c
marlon <kurtyebsb@gmail.com>
2017-11-17 16:23:02 -0200

    tipagem dos dados

--- src/models/post.ts
+++ src/models/post.ts
@@ -2,4 +2,10 @@ export interface Post {
     nome: string;
     especie: string;
     whatsapp: string;
+    informacao: string;
+    idade: string;
+    sexo: string;
+    raca: string;
+    fotoUrl: string;
+
 }
