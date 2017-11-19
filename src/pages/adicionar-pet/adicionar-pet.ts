1394153eff6726e9530b6dedaf52eab72963fa73
rubenalmeida <ruben36almeida96@gmail.com>
2017-11-19 13:01:44 -0200

    upload foto junto com o form

--- src/pages/adicionar-pet/adicionar-pet.ts
+++ src/pages/adicionar-pet/adicionar-pet.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {NavController, AlertController, NavParams} from 'ionic-angular';
-
+import {TabsControllerPage} from "../tabs-controller/tabs-controller";
 import {AngularFireDatabase} from 'angularfire2/database'
 import {Post} from "../../models/post";
 import {Camera, CameraOptions} from "@ionic-native/camera";
@@ -16,9 +16,9 @@ import {storage} from "firebase";
 export class AdicionarPetPage {
 
   post = {} as Post;
+  photoUrl: string = '';
 
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController,
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alert: AlertController,
               private camera: Camera, private afDatabase: AngularFireDatabase) {
     console.log(this.post);
   }
@@ -37,10 +37,18 @@ export class AdicionarPetPage {
         correctOrientation: true
       }
 
-      await this.camera.getPicture(options);
-      const image = 'data:image/jpeg;base64,${result}';
-      const picture = storage().ref('images/adocao/');
-      picture.putString(image, 'data_url');
+      // await this.camera.getPicture(options);
+      // const image = 'data:image/jpeg;base64,${result}';
+      // console.log(image);
+
+      await this.camera.getPicture(options).then((imageData) => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64:
+        let base64Image = 'data:image/jpeg;base64,' + imageData;
+        this.photoUrl = base64Image;
+      }, (err) => {
+        // Handle error
+      });
     }
     catch (e) {
       console.error(e);
@@ -50,7 +58,23 @@ export class AdicionarPetPage {
 
 
   addPost() {
-    this.afDatabase.list('BR/adocao/pets').push(this.post);
+    try {
+      this.afDatabase.list('BR/adocao/pets').push(this.post).then(res => {
+        const picture = storage().ref('images/adocao/');
+        picture.putString(this.photoUrl, 'data_url');
+        console.log(res, 'pet cadastrado');
+        let popup = this.alert.create({
+          title: 'Pet Postado',
+          subTitle: 'Boa sorte!',
+          buttons: ['Dismiss']
+        });
+        popup.present();
+      });
+      this.navCtrl.push(TabsControllerPage);
+    } catch (e) {
+      console.log(e);
+    }
+
   }
 
 }
