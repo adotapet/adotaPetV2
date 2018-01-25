0e30550be6220daa36af57f08e70031c554686bb
rubenalmeida <ruben36almeida96@gmail.com>
2018-01-25 19:34:01 -0200

    listando os meus pets(nao testado, so acreditando que ta funcionando)

--- src/pages/meus-pets/meus-pets.ts
+++ src/pages/meus-pets/meus-pets.ts
@@ -1,6 +1,10 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, ToastController} from 'ionic-angular';
 import {AuthProvider} from "../../providers/auth/auth";
+import {GoogleAnalytics} from "@ionic-native/google-analytics";
+import {AngularFireAuth} from "angularfire2/auth";
+import {AngularFireDatabase} from "angularfire2/database";
+import {NativePageTransitions} from "@ionic-native/native-page-transitions";
 
 @Component({
     selector: 'page-meus-pets',
@@ -9,13 +13,29 @@ import {AuthProvider} from "../../providers/auth/auth";
 export class MeusPetsPage {
 
     myPets = [];
+    myId: string;
 
-    constructor(public navCtrl: NavController, public auth: AuthProvider) {
+    constructor(private afAuth: AngularFireAuth,
+                private db: AngularFireDatabase,
+                public navCtrl: NavController,
+                public toast: ToastController,
+                private nativePageTransitions: NativePageTransitions,
+                public ga: GoogleAnalytics,
+                private authProvider: AuthProvider) {
+
     }
 
-    ionViewDidLoad(){
-
+    ionViewDidLoad() {
+        this.ga.trackView('meus-pets');
+        this.myId = this.authProvider.getUser().uid;
+        this.listPets();
     }
 
+    async listPets() {
+        this.db.list('BR/adocao/pets', ref => ref.orderByChild('user').equalTo(this.myId)).snapshotChanges().subscribe(data => {
+            this.myPets = data;
+            console.log(data);
+        });
+    }
 
 }
